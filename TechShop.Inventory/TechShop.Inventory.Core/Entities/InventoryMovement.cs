4158747954abe3295dab9af79653e79f1f2b3c67
skinofme <skinoffme@gmail.com>
2025-12-16 10:03:56 -0600

    Add InventoryMovement domain entity

---
+++ TechShop.Inventory/TechShop.Inventory.Core/Entities/InventoryMovement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechShop.Inventory.Core.Enums.InventoryMovement;
+
+namespace TechShop.Inventory.Core.Entities
+{
+	public class InventoryMovement
+	{
+		public int IdInventoryMovement { get; private set; }
+
+		public int IdStockItem {  get; private set; }
+
+		public MovementType MovementType { get; private set; }
+		public int Quantity { get; private set; }
+		public string Reason {  get; private set; }
+
+		public string ReferenceId { get; private set; }
+
+
+
+		// Constructor to create a new entity
+		public InventoryMovement(int idStockItem, MovementType movementType, int quantity, string reason, string referenceId)
+		{
+			IdStockItem = idStockItem;
+			MovementType = movementType;
+			Quantity = quantity;
+			Reason = reason;
+			ReferenceId = referenceId;
+		}
+
+		// Constructor to rehydrate a entity
+		public InventoryMovement(int idInventoryMovement, int idStockItem, MovementType movementType, int quantity, string reason, string referenceId)
+		{
+			IdInventoryMovement = idInventoryMovement;
+			IdStockItem = idStockItem;
+			MovementType = movementType;
+			Quantity = quantity;
+			Reason = reason;
+			ReferenceId = referenceId;
+		}
+	}
+}
