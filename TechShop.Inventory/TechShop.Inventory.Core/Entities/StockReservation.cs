d3ad8389807031244357eaefa630efcb0ce3b05d
skinofme <skinoffme@gmail.com>
2025-12-31 23:01:58 -0600

    feat(Core): Add StockReservation domain entity

---
+++ TechShop.Inventory/TechShop.Inventory.Core/Entities/StockReservation.cs
@@ -0,0 +1,121 @@
+﻿using System.Linq.Expressions;
+using TechShop.Inventory.Core.Enums.StockReservation;
+using TechShop.Inventory.Core.Exceptions.common;
+using TechShop.Inventory.Core.Exceptions.StockReservation;
+
+namespace TechShop.Inventory.Core.Entities
+{
+	public class StockReservation
+	{
+		public int IdStockReservation { get; private set; }
+
+		public int IdStockItem { get; private set; }
+
+		public int Quantity { get; private set; }
+
+		public DateTime CreatedAt { get; private set; }
+
+		public DateTime ExpiresAt { get; private set; }
+
+		public ReservationStatus Status { get; private set; }
+
+		public string Reason { get; private set; }
+
+		public string ReferenceId { get; private set; }
+
+		protected StockReservation() { }
+
+		// constructor to create a new entity
+		public StockReservation(
+			int idStockItem,
+			int quantity,
+			DateTime createdAt,
+			DateTime expiresAt,
+			string reason,
+			string referenceId)
+		{
+			ValidateConstructor(idStockItem, quantity, createdAt, expiresAt, reason, referenceId);
+
+			IdStockItem = idStockItem;
+			Quantity = quantity;
+			CreatedAt = createdAt;
+			ExpiresAt = expiresAt;
+			Status = ReservationStatus.PENDING;
+			Reason = reason;
+			ReferenceId = referenceId;
+		}
+
+		// constructor to rehydrate a entity
+		internal StockReservation(
+			int idStockReservation,
+			int idStockItem,
+			int quantity,
+			DateTime createdAt,
+			DateTime expiresAt,
+			ReservationStatus status,
+			string reason,
+			string referenceId)
+		{
+			if (idStockReservation <= 0) throw new InvalidIdStockReservationException(idStockReservation);
+			ValidateConstructor(idStockItem, quantity, createdAt, expiresAt, reason, referenceId);
+
+			IdStockReservation = idStockReservation;
+			IdStockItem = idStockItem;
+			Quantity = quantity;
+			CreatedAt = createdAt;
+			ExpiresAt = expiresAt;
+			Status = status;
+			Reason = reason;
+			ReferenceId = referenceId;
+		}
+
+		// CONFIRMED
+		public void Confirm(DateTime now)
+		{
+			if (Status == ReservationStatus.CONFIRMED) return; // Idempotency
+
+			if (Status != ReservationStatus.PENDING) 
+				throw new InvalidStockReservationStateException(Status, "Confirm");
+
+			if (ExpiresAt <= now) throw new StockReservationExpiredException(ExpiresAt, now);
+
+			Status = ReservationStatus.CONFIRMED;
+		}
+
+		// CANCELLED
+		public void Cancel()
+		{
+			if (Status == ReservationStatus.CANCELLED || Status == ReservationStatus.EXPIRED) // Idempotency
+				return;
+
+			if (Status != ReservationStatus.PENDING) 
+				throw new InvalidStockReservationStateException(Status, "Cancel");
+
+			Status = ReservationStatus.CANCELLED;
+		}
+
+		// EXPIRED
+		public void Expire(DateTime now)
+		{
+			if (Status == ReservationStatus.EXPIRED || Status == ReservationStatus.CANCELLED) return; // Idempotency
+
+			if (Status != ReservationStatus.PENDING)
+				throw new InvalidStockReservationStateException(Status, "Expire"); // Only pending reservation can be expired
+
+			if (ExpiresAt > now )
+				throw new StockReservationNotExpiredException(ExpiresAt, now); // It cannot be expire before
+
+			Status = ReservationStatus.EXPIRED;
+		}
+
+		private void ValidateConstructor(int idStockItem, int quantity, DateTime createdAt, DateTime expiresAt, string reason, string referenceId)
+		{
+			if (idStockItem <= 0) throw new InvalidIdStockItemException(idStockItem);
+			if (quantity <= 0) throw new InvalidQuantityException(quantity);
+			if (expiresAt <= createdAt) throw new InvalidExpirationDateException(expiresAt, createdAt);
+			if (string.IsNullOrWhiteSpace(reason)) throw new InvalidReasonException(reason);
+			if (string.IsNullOrWhiteSpace(referenceId)) throw new InvalidReferenceIdException(referenceId);
+		}
+
+	}
+}
---
+++ TechShop.Inventory/TechShop.Inventory.Core/Enums/StockReservation/ReservationStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace TechShop.Inventory.Core.Enums.StockReservation
+{
+	public enum ReservationStatus
+	{
+		PENDING,
+		CONFIRMED,
+		CANCELLED,
+		EXPIRED
+	}
+}
