7515017e6ace30cef6628c5bf0cb5b93d6e0b29a
skinofme <skinoffme@gmail.com>
2026-01-02 19:06:18 -0600

    feat(Core): Add StockItem domain exception

---
+++ TechShop.Inventory/TechShop.Inventory.Core/Exceptions/StockItem/StockReservationNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace TechShop.Inventory.Core.Exceptions.StockItem
+{
+	public class StockReservationNotFoundException : DomainException
+	{
+		public StockReservationNotFoundException(int idStockReservation)
+			: base($"Stock reservation not found. IdStockReservation: {idStockReservation}")
+		{
+		}
+	}
+}
